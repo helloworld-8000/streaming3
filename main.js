6c8245804bac597c7b7a7843144c51cf7ec3e4f0
helloworld-8000 <a.n.d.i.m.a.r.i.a.d.i.1995@gmail.com>
2025-01-27 10:48:25 +0800

    Update main.js

--- main.js
+++ main.js
@@ -10,26 +10,29 @@ const audio = "streaming.mp3";
 const ffmpegCommand = [
   'ffmpeg',
   '-stream_loop', '-1',  // Loop video
-  '-i', video,
-  '-stream_loop', '-1',  // Loop audio
-  '-i', audio,
+  '-re',
+  '-i', video,            // Ganti dengan path video Anda
+  '-stream_loop', '-1',   // Loop audio
+  '-re',
+  '-i', audio,            // Ganti dengan path audio Anda
   '-vcodec', 'libx264',
-  '-pix_fmt', 'yuv420p',  // Gunakan pix_fmt 'yuv420p' untuk kompatibilitas lebih baik
-  '-maxrate', '6000k',   // Sesuaikan bitrate maksimum video
-  '-bufsize', '6000k',   // Tambahkan buffer size untuk stabilitas
-  '-preset', 'veryfast', // Gunakan preset 'veryfast' untuk streaming
-  '-r', '30',            // Tingkatkan framerate menjadi 30fps
-  '-g', '60',            // Group of Pictures (GOP) = 2x framerate
-  '-crf', '23',          // Kurangi CRF untuk kualitas lebih baik
+  '-pix_fmt', 'yuv420p',   // Gunakan pix_fmt 'yuv420p' untuk kompatibilitas lebih baik
+  '-maxrate', '3500k',     // Turunkan bitrate maksimum video (contoh: 3500 kbps)
+  '-bufsize', '3500k',     // Sesuaikan buffer size agar lebih stabil
+  '-preset', 'veryfast',   // Gunakan preset 'veryfast' untuk streaming
+  '-r', '30',              // Tingkatkan framerate menjadi 30fps
+  '-g', '60',              // Group of Pictures (GOP) = 2x framerate
+  '-crf', '23',            // Kurangi CRF untuk kualitas lebih baik
   '-c:a', 'aac',
-  '-b:a', '192k',        // Bitrate audio lebih tinggi
+  '-b:a', '192k',          // Bitrate audio lebih tinggi
   '-ar', '44100',
   '-strict', 'experimental',
   '-f', 'flv',
-  '-map', '0:v:0',  // Map video stream dari input pertama (video file)
-  '-map', '1:a:0',  // Map audio stream dari input kedua (audio file)
+  '-map', '0:v:0',         // Map video stream dari input pertama (video file)
+  '-map', '1:a:0',         // Map audio stream dari input kedua (audio file)
   `rtmp://a.rtmp.youtube.com/live2/${streamkey}`,
 ];
+
 
 const child = spawn(ffmpegCommand[0], ffmpegCommand.slice(1));
 
